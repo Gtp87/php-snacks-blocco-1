fbf80979967cf39f856c658db7535ae2ed3ecdc5
Gtp87 <fabio.giannitrapani@gmail.com>
2022-01-31 15:36:52 +0100

    snack5

--- snack5.php
+++ snack5.php
@@ -1,4 +1,24 @@
 <?php
 // ## Snack 5
 // Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+$paragraph = 'Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.';
+
+// divido la stringa ad ogni punto
+$divided = explode('. ', $paragraph);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- stampo i paragrafi divisi -->
+    <?php foreach ($divided as $key => $value) { ?>
+        <p> <?php echo $value;?></p>
+    <?php } ?>
+</body>
+</html>
